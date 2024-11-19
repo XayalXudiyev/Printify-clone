0faafabd067c09881506da6b154d906b089df88f
XayalXudiyev <xayal.xuduyev@gmail.com>
2024-11-20 03:03:10 +0400

    bugs fixed

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,42 +1,47 @@
-import { prismadb } from "@/lib/prismadb"
-import sharp from "sharp"
-import { type FileRouter, createUploadthing } from "uploadthing/next"
-import { z } from "zod"
+import { prismadb } from "@/lib/prismadb";
+import sharp from "sharp";
+import { type FileRouter, createUploadthing } from "uploadthing/next";
+import { z } from "zod";
 
-const f = createUploadthing()
-
-const auth = (req: Request) => ({ id: "fakeId" })
+const f = createUploadthing({
+  token: process.env.UPLOADTHING_TOKEN
+}); 
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .input(z.object({ configId: z.string().optional() }))
     .middleware(async ({ input }) => {
-      return { input }
+      return { input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input
-      const res = await fetch(file.url)
-      const buffer = await res.arrayBuffer()
-      const imgMetadata = await sharp(buffer).metadata()
-      const { height, width } = imgMetadata
+      try {
+        const { configId } = metadata.input;
+        const res = await fetch(file.url);
+        const buffer = Buffer.from(await res.arrayBuffer());
+        const imgMetadata = await sharp(buffer).metadata();
+        const { height, width } = imgMetadata;
 
-      if (!configId) {
-        const configuration = await prismadb.configuration.create({
-          data: {
-            imageUrl: file.url,
-            height: height || 500,
-            width: width || 500,
-          },
-        })
-        return { configId: configuration.id }
+        if (!configId) {
+          const configuration = await prismadb.configuration.create({
+            data: {
+              imageUrl: file.url,
+              height: height || 500,
+              width: width || 500,
+            },
+          });
+          return { configId: configuration.id };
+        }
+
+        const updateConfiguration = await prismadb.configuration.update({
+          where: { id: configId },
+          data: { croppedImageUrl: file.url },
+        });
+        return { configId: updateConfiguration.id };
+      } catch (error) {
+        console.error("Upload erreeeeeeeeeeeeeeeor:", error);
+        throw new Error("Upload failed");
       }
-
-      const updateConfiguration = await prismadb.configuration.update({
-        where: { id: configId },
-        data: { croppedImageUrl: file.url },
-      })
-      return { configId: updateConfiguration.id }
     }),
-} satisfies FileRouter
+} satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter
+export type OurFileRouter = typeof ourFileRouter;
